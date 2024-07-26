40bfa0bbe6466f09ada9859140ff4e4b70523e7c
uyuuuuu <soufflesumire.03@gmail.com>
2024-07-26 23:14:01 +0900

    update

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,28 @@
+// The MIT License
+
+// Copyright © 2010-2024 three.js authors
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+// 22FI108
+// 三木すみれ
+    
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
@@ -143,7 +168,7 @@ class SakuraScene {
         const grassCount = 100000;
         const grassGeometry = new THREE.PlaneGeometry(0.3, 1.5);
         this.grassMaterial = this.createGrassMaterial();
-        // 草が多いからインスタンス化
+        // 草のインスタンス化
         this.grassInstances = new THREE.InstancedMesh(grassGeometry, this.grassMaterial, grassCount);
 
         const matrix = new THREE.Matrix4();
@@ -186,6 +211,7 @@ class SakuraScene {
             if (position.y < 0) {
                 position.y = 50; //上に戻す
             }
+            // ゆらぎ
             position.x += Math.sin(Date.now() * 0.001 + i) * 0.01;
 
             rotation.setFromQuaternion(quaternion);
