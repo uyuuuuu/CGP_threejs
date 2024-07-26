0657c86e717a137b035d3372081f1bbfb90cccff
uyuuuuu <soufflesumire.03@gmail.com>
2024-07-26 19:45:14 +0900

    first

---
+++ src/app.ts
@@ -0,0 +1,78 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+class ThreeJSContainer {
+    private scene: THREE.Scene;
+    private geometry: THREE.BufferGeometry;
+    private material: THREE.Material;
+    private cube: THREE.Mesh;
+    private light: THREE.Light;
+
+    constructor() {
+
+    }
+
+    // 画面部分の作成(表示する枠ごとに)*
+    public createRendererDOM = (width: number, height: number, cameraPos: THREE.Vector3) => {
+        let renderer = new THREE.WebGLRenderer();
+        renderer.setSize(width, height);
+        renderer.setClearColor(new THREE.Color(0x495ed));
+
+        //カメラの設定
+        let camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+        camera.position.copy(cameraPos);
+        camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+        let orbitControls = new OrbitControls(camera, renderer.domElement);
+
+        this.createScene();
+        // 毎フレームのupdateを呼んで，render
+        // reqestAnimationFrame により次フレームを呼ぶ
+        let render: FrameRequestCallback = (time) => {
+            orbitControls.update();
+
+            renderer.render(this.scene, camera);
+            requestAnimationFrame(render);
+        }
+        requestAnimationFrame(render);
+
+        renderer.domElement.style.cssFloat = "left";
+        renderer.domElement.style.margin = "10px";
+        return renderer.domElement;
+    }
+
+    // シーンの作成(全体で1回)
+    private createScene = () => {
+        this.scene = new THREE.Scene();
+
+        this.geometry = new THREE.BoxGeometry(1, 1, 1);
+        this.material = new THREE.MeshLambertMaterial({ color: 0x55ff00 });
+        this.cube = new THREE.Mesh(this.geometry, this.material);
+        this.cube.castShadow = true;
+        this.scene.add(this.cube);
+
+        //ライトの設定
+        this.light = new THREE.DirectionalLight(0xffffff);
+        let lvec = new THREE.Vector3(1, 1, 1).normalize();
+        this.light.position.set(lvec.x, lvec.y, lvec.z);
+        this.scene.add(this.light);
+
+        // 毎フレームのupdateを呼んで，更新
+        // reqestAnimationFrame により次フレームを呼ぶ
+        let update: FrameRequestCallback = (time) => {
+            this.cube.rotateX(0.01);
+
+            requestAnimationFrame(update);
+        }
+        requestAnimationFrame(update);
+    }
+}
+
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    let container = new ThreeJSContainer();
+
+    let viewport = container.createRendererDOM(640, 480, new THREE.Vector3(-3, 3, 3));
+    document.body.appendChild(viewport);
+}
